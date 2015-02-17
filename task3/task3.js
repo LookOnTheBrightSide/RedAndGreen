58fea167d7ac4e23ec902287fab7943c2970d845
LookOnTheBrightSide <nkosindlovu87@gmail.com>
2015-02-17 14:21:45 +0200

    task 3 is done

---
+++ task3/task3.js
@@ -0,0 +1,18 @@
+function changeColor2(){
+
+	
+		var red = document.getElementById('box').style.backgroundColor = '#ff0000';
+		
+	
+
+}
+function changeColor(){
+
+	
+		
+	
+		var green = document.getElementById('box').style.backgroundColor = '#00ff00';
+		
+	
+
+}
